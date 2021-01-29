4f329bb6d717e4aa6178c50eafa59e17137fbcd5
Alan Call <alanmatthewcall@gmail.com>
2021-01-29 13:19:02 -0500

    add buisness logic for Order constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
 
-//BuinesS Logic
+//Buiness Logic
 
-function Order() {
+function Order(firstName, lastName,) {
   this.firstName = firstName;
   this.lastName = lastName;
-  this.pizzaSize = pizzaSize;
-  this.pizzaToppings = pizzaToppings;
+  this.pizzaToppings = pizzaToppings(toppings1, toppings2);
   this.cartTotal = 0;
   this.tax = 1;
 }  
+
+let firstName = [];
+let lastName = [];
 
 Order.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
 Order.prototype.checkoutTotal = function() {
-  return "Your"
-}
+  return "Your Total Is " + this.cartTotal + this.tax + " Please Click Submit To Complete Your Order"
+};
+
+
+
+
+
+  
