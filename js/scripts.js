8627010581502ca48001d74931e701988c3a793f
Alan Call <alanmatthewcall@gmail.com>
2021-01-29 16:33:00 -0500

    add more HTML and attempt to link JS functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,28 @@
 
 //Buiness Logic
 
+let topping1 = 1;
+let topping2 = 1;
+
 function Order(firstName, lastName,) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.pizzaToppings = pizzaToppings ["topping1", "topping2"];
   this.cartTotal = 0;
-  this.tax = 1;
 }  
 
-Order.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
+function pizza() {
+  this.price
+  this.qty
 }
 
+//Order.prototype.fullName = function() {
+ // return this.firstName + " " + this.lastName;
+//}
+
 Order.prototype.checkoutTotal = function() {
-  return "Your Total Is " + this.cartTotal + this.tax + " Please Click Submit To Complete Your Order"
+  totalCost = cartTotal + this.pizzaToppings;
+  return "Your Total Is " + totalCost +  " Please Click Submit To Complete Your Order"
 };
 
 // UI Logic
@@ -24,10 +32,11 @@ $(document).ready(function () {
     $("#nineteen").hide();
     $("#eighteen").hide(); 
     $("#seventeen").hide();
+    const this.firstName = $("input#firstName").val();
+    const this.lastName = $("input#lastName").val();
     const toppingsOne = parseInt($("input:radio[name=creative]:checked").val());
     const toppingsTwo = parseInt($("input:radio[name=design]:checked").val());
-    
-    event.preventDefault();
+    $("#show-cart").slideDown();
     if (toppingsOne + toppingsTwo === 2) {
       $("#nineteen").show();
     } else if (toppingsOne + toppingsTwo === 1) {
