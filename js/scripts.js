dc8c4ae5506bb8d64f8081355ca6b6e2810fb9ff
Alan Call <alanmatthewcall@gmail.com>
2021-01-29 17:04:15 -0500

    update js scripts file, delete first and last name properties, simplify properties

--- js/scripts.js
+++ js/scripts.js
@@ -4,46 +4,34 @@
 let topping1 = 1;
 let topping2 = 1;
 
-function Order(firstName, lastName,) {
-  this.firstName = firstName;
-  this.lastName = lastName;
+function Order(pizzaToppings, pizzaSize) {
   this.pizzaToppings = pizzaToppings ["topping1", "topping2"];
+  this.pizzaSize = pizzaSize;
   this.cartTotal = 0;
 }  
 
-function pizza() {
-  this.price
-  this.qty
-}
 
-//Order.prototype.fullName = function() {
- // return this.firstName + " " + this.lastName;
-//}
+
+Order.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
 
 Order.prototype.checkoutTotal = function() {
   totalCost = cartTotal + this.pizzaToppings;
-  return "Your Total Is " + totalCost +  " Please Click Submit To Complete Your Order"
+  return fullName + " Your Total Is " + totalCost +  " Please Click Submit To Complete Your Order";
 };
 
 // UI Logic
 
 $(document).ready(function () {
-  $("#pizzaParlor").submit(function (event) {
-    $("#nineteen").hide();
-    $("#eighteen").hide(); 
-    $("#seventeen").hide();
+  $("#pizzaParlor").submit(function(event) {
+    event.preventDefault();
     const this.firstName = $("input#firstName").val();
     const this.lastName = $("input#lastName").val();
     const toppingsOne = parseInt($("input:radio[name=creative]:checked").val());
     const toppingsTwo = parseInt($("input:radio[name=design]:checked").val());
-    $("#show-cart").slideDown();
-    if (toppingsOne + toppingsTwo === 2) {
-      $("#nineteen").show();
-    } else if (toppingsOne + toppingsTwo === 1) {
-      $("#eighteen").show();
-    } else (toppingsOne + toppingsTwo === 0) {
-      $("#seventeen").show();
-    }
+    let newOrder = new Order(fullName, totalCost)
+    $("#show-cart").show();
   });
 });
 
